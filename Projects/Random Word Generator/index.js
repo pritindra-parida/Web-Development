e7cad0dd092d14f10cbda5df8fecf4c00172967d
Pritindra <pritindra100@gmail.com>
2023-06-03 16:17:39 +0530

    Random Word Generator

--- Projects/Random Word Generator/index.js
+++ Projects/Random Word Generator/index.js
@@ -10,29 +10,44 @@ function randomString(len){
   }
   return word;
 }
-document.querySelector("h1").innerHTML=randomWord;
+$("h1").html(randomWord);
 
 
-function keyResponse(key, count){
-  var audioSource = "sounds/s" + (Math.floor(Math.random()*7)+1) + ".mp3";
-  var sound = new Audio(audioSource);
 
-  if(count>=randomLength){
-    location.reload();
+// function keyResponse(key, count){
+//   var audioSource = "sounds/s" + (Math.floor(Math.random()*7)+1) + ".mp3";
+//   var sound = new Audio(audioSource);
+//   if(count>=randomLength){
+//     location.reload();
+//   }
+//   if(key==randomWord[count]){
+//     sound.play();
+//   }else {
+//     var error = new Audio("sounds/error.mp3");
+//     error.play();
+//     count--;
+//   }
+// }
+
+function keyResponse(key){
+  if(key==randomWord[0]){
+    $("h1").addClass("pressed");
+    setTimeout(function(){
+      $("h1").removeClass("pressed");
+    },300);
   }
-  if(key==randomWord[count]){
-    sound.play();
-  }else {
-    var error = new Audio("sounds/error.mp3");
-    error.play();
-    count--;
+  else{
+    $("h1").addClass("wrongKey");
+    setTimeout(function(){
+      $("h1").removeClass("wrongKey");
+    },300);
   }
 }
 
 
 document.addEventListener("keydown",function(event){
   count++;
-  keyResponse(event.key, count);
+  keyResponse(event.key);
 });
 
 
